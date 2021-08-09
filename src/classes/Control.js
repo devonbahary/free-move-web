4dea87de050b749ff3298de447e5e63c1159da4d
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-08-08 23:26:06 -0700

    adds 8-dir movement

--- src/classes/Control.js
+++ src/classes/Control.js
@@ -3,7 +3,9 @@ import { Vectors } from "../utilities/Vectors";
 export class Control {
     constructor(world) {
         document.onkeydown = this.onkeydown;
+        document.onkeyup = this.onkeyup;
         this.world = world;
+        this.pressedKeys = {};
     }
 
     get player() {
@@ -11,26 +13,51 @@ export class Control {
     }
 
     onkeydown = (keyboardEvent) => {
-        const movement = 0.1;
+        this.pressedKeys[keyboardEvent.key] = true;
+
         switch (keyboardEvent.key) {
             case 'ArrowRight':
-                keyboardEvent.preventDefault();
-                this.player.move(Vectors.create(movement, 0));
-                break;
             case 'ArrowUp':
-                keyboardEvent.preventDefault();
-                this.player.move(Vectors.create(0, -movement));
-                break;
             case 'ArrowLeft':
-                keyboardEvent.preventDefault();
-                this.player.move(Vectors.create(-movement, 0));
-                break;
             case 'ArrowDown':
                 keyboardEvent.preventDefault();
-                this.player.move(Vectors.create(0, movement));
                 break;
             default:
                 break;
         }
+
+        const movementVector = Vectors.create(0, 0);
+
+        if (this.pressedKeys['ArrowUp']) {
+            movementVector.y = -1;
+            if (this.pressedKeys['ArrowLeft']) {
+                movementVector.x = -1;
+            } else if (this.pressedKeys['ArrowRight']) {
+                movementVector.x = 1;
+            }
+        } else if (this.pressedKeys['ArrowRight']) {
+            movementVector.x = 1;
+            if (this.pressedKeys['ArrowDown']) {
+                movementVector.y = 1;
+            }
+        } else if (this.pressedKeys['ArrowDown']) {
+            movementVector.y = 1;
+            if (this.pressedKeys['ArrowLeft']) {
+                movementVector.x = -1;
+            }
+        } else if (this.pressedKeys['ArrowLeft']) {
+            movementVector.x = -1;
+        }
+
+        if (Vectors.magnitude(movementVector)) {
+            const movement = 0.1;
+            const normalizedVector = Vectors.normalize(movementVector);
+            this.player.move(Vectors.mult(normalizedVector, movement));
+        }
+    }
+
+    onkeyup = (keyboardEvent) => {
+        console.log(keyboardEvent.key);
+        delete this.pressedKeys[keyboardEvent.key];
     }
 }
