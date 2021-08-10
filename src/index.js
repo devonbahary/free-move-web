f2dc1aee70aa0c1f0597a953e8d158142cf54a80
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-08-10 12:39:30 -0700

    adds to-do list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import { game, gameParams} from './classes/Game';
 import './main.css';
 
 for (let i = 0; i < 3; i++) {
+/*
+    to-do list:
+        - inelastic collisions with another circle / rectangle
+            - implement "sliding" or "glancing" 
+        - investigate weird "jumping" / teleporting behavior when moving immobile elastic entities
+        - apply force instead of just setting velocity in move()
+*/
+
     const character = new Character();
     const randX = Math.floor((Math.random() * gameParams.bounds.width));
     const randY = Math.floor((Math.random() * gameParams.bounds.height));
