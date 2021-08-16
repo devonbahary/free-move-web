c6559681ff97f4b2909ed644b1b72cd3854cef1d
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-08-15 20:28:29 -0700

    game set up refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { Character } from './classes/entities/Character';
 import { game, gameParams} from './classes/Game';
+import { Vectors } from './utilities/Vectors';
 import './main.css';
 
-for (let i = 0; i < 3; i++) {
 /*
     to-do list:
         - inelastic collisions with another circle / rectangle
@@ -11,13 +11,42 @@ for (let i = 0; i < 3; i++) {
         - apply force instead of just setting velocity in move()
 */
 
-    const character = new Character();
-    const randX = Math.floor((Math.random() * gameParams.bounds.width));
-    const randY = Math.floor((Math.random() * gameParams.bounds.height));
-    character.moveTo(randX, randY);
-    game.world.addCharacter(character);
-}
+const GAME_MODES = {
+    NORMAL: 'NORMAL',
+    CHAOS: 'CHAOS',
+};
+
+const gameMode = GAME_MODES.NORMAL;
 
 setInterval(() => {
     game.update();
 }, 1000 / 60);
+
+seedCharacters();
+gameSetup();
+
+const gameSetup = () => {
+    switch (gameMode) {
+        case GAME_MODES.NORMAL:
+    
+            break;
+        case GAME_MODES.CHAOS:
+            for (const character of game.world.characters) {
+                character.move(Vectors.create(Math.random() * 1, Math.random() * 1));
+            }
+            break;
+        default:
+            break;
+    }
+    console.log(game);
+};
+
+const seedCharacters = () => {
+    for (let i = 0; i < 5; i++) {
+        const character = new Character();
+        const randX = Math.floor((Math.random() * gameParams.bounds.width));
+        const randY = Math.floor((Math.random() * gameParams.bounds.height));
+        character.moveTo(randX, randY);
+        game.world.addCharacter(character);
+    }
+};
