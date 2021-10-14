80d934ce9bf54f2e4774c5025c5dc85d504bd1d1
Devon Bahary <devon.bahary@robinhealthcare.com>
2021-10-14 13:12:24 -0700

    removes Body.update() by moving responsibility of managing bodies to World

--- src/classes/entities/Bodies.js
+++ src/classes/entities/Bodies.js
@@ -1,12 +1,9 @@
 import { v4 as uuid } from 'uuid';
-import { Collisions } from "../../utilities/Collisions";
 import { Vectors } from "../../utilities/Vectors";
-
-const isCircle = (body) => body.hasOwnProperty('radius');
 
 // should be abstract -- cannot work with an object if we don't know if it's
 // a circle or rectangle
-class Body {
+export class Body {
     constructor() {
         this.id = uuid();
         this.x = 0;
@@ -25,6 +22,10 @@ class Body {
             other collisions or move uninhibited
         */
         this.collisionPartner = null; 
+    }
+
+    static isCircle(body) {
+        return body.hasOwnProperty('radius');
     }
 
     isMoving() {
@@ -67,8 +68,6 @@ export class RectangleBody extends Body {
     get y1() {
         return this.y + this.height;
     }
-
-    update() {}
 }
 
 export class CircleBody extends Body {
@@ -95,84 +94,5 @@ export class CircleBody extends Body {
 
     get y1() {
         return this.y + this.radius * 2;
-    }
-
-    update(world) {
-        this.updatePosition(world);
-    }
-
-    updatePosition(world) {
-        if (!this.isMoving()) {
-            return;
-        }
-
-        const moveToPointOfCollision = (timeOfCollision) => {
-            const dx = this.velocity.x * timeOfCollision;
-            const dy = this.velocity.y * timeOfCollision;
-            this.moveTo(this.x + dx, this.y + dy);
-        };
-
-        const movementBoundingBox = Collisions.getMovementBoundingBox(this);
-
-        let wasCollision = false;
-
-        // TODO: what if multiple collisions possible in movement and should react to closest?
-        for (const body of world.bodies) {
-            if (body === this || body === this.collisionPartner) {
-                continue;
-            }
-
-            if (isCircle(body)) {
-                const vectorToRect = Vectors.getClosestVectorToRectFromCircle(body, movementBoundingBox);
-                const distanceToRectangle = Vectors.magnitude(vectorToRect);
-                
-                if (Collisions.isCircleCollidedWithRectangle(distanceToRectangle, this.radius)) {
-                    const timeOfCollision = Collisions.getTimeOfCircleOnCircleCollision(this, body);
-
-                    if (timeOfCollision !== null) {
-                        moveToPointOfCollision(timeOfCollision);
-
-                        if (this.isElastic && body.isElastic) {
-                            const [
-                                finalVelocity,
-                                bodyFinalVelocity,
-                            ] = Collisions.resolveElasticCircleOnCircleCollision(this, body);
-
-                            this.setVelocity(finalVelocity);
-                            body.setVelocity(bodyFinalVelocity);
-
-                            body.collisionPartner = this;
-                        }
-
-                        wasCollision = true;
-                        break;
-                    }
-                    
-                }
-            } else { // isRectangle
-                if (Collisions.areRectanglesColliding(movementBoundingBox, body)) {
-                    const timeOfCollision = Collisions.getTimeOfCircleOnRectangleCollision(this, body);
-
-                    if (timeOfCollision !== null) {
-                        moveToPointOfCollision(timeOfCollision);
-
-                        if (this.isElastic) { // assume all rectangles are inelastic
-                            const newVector = Collisions.resolveElasticCircleOnInelasticRectangleCollision(this, body);
-                            this.setVelocity(newVector);
-                        }
-                        
-                        wasCollision = true;
-                        break;
-                    }
-                }
-            }
-        }
-        
-        if (!wasCollision) {
-            this.x = this.x + this.velocity.x;
-            this.y = this.y + this.velocity.y;
-        }
-
-        this.collisionPartner = null;
     }
 }
--- src/classes/entities/World.js
+++ src/classes/entities/World.js
@@ -1,4 +1,12 @@
-import { RectangleBody } from "./Bodies";
+import { Collisions } from "../../utilities/Collisions";
+import { Vectors } from "../../utilities/Vectors";
+import { Body, RectangleBody } from "./Bodies";
+
+const moveBodyToPointOfCollision = (body, timeOfCollision) => {
+    const dx = body.velocity.x * timeOfCollision;
+    const dy = body.velocity.y * timeOfCollision;
+    body.moveTo(body.x + dx, body.y + dy);
+};
 
 export class World {
     constructor(bounds) {
@@ -37,8 +45,75 @@ export class World {
     }    
 
     update() {
-        for (const body of this.bodies) {
-            body.update(this);
+        for (const actingBody of this.bodies) {
+            if (!actingBody.isMoving()) {
+                continue;
+            }
+    
+            const movementBoundingBox = Collisions.getMovementBoundingBox(actingBody);
+    
+            let wasCollision = false;
+    
+            // TODO: implement quadtree to prevent O(n^2)
+            // TODO: what if multiple collisions possible in movement and should react to closest?
+            for (const otherBody of this.bodies) {
+                if (otherBody === actingBody || otherBody === actingBody.collisionPartner) {
+                    continue;
+                }
+    
+                if (Body.isCircle(otherBody)) {
+                    const vectorToRect = Vectors.getClosestVectorToRectFromCircle(otherBody, movementBoundingBox);
+                    const distanceToRectangle = Vectors.magnitude(vectorToRect);
+                    
+                    if (Collisions.isCircleCollidedWithRectangle(distanceToRectangle, actingBody.radius)) {
+                        const timeOfCollision = Collisions.getTimeOfCircleOnCircleCollision(actingBody, otherBody);
+    
+                        if (timeOfCollision !== null) {
+                            moveBodyToPointOfCollision(actingBody, timeOfCollision);
+    
+                            if (actingBody.isElastic && otherBody.isElastic) {
+                                const [
+                                    finalVelocity,
+                                    bodyFinalVelocity,
+                                ] = Collisions.resolveElasticCircleOnCircleCollision(actingBody, otherBody);
+    
+                                actingBody.setVelocity(finalVelocity);
+                                otherBody.setVelocity(bodyFinalVelocity);
+    
+                                otherBody.collisionPartner = actingBody;
+                            }
+    
+                            wasCollision = true;
+                            break;
+                        }
+                        
+                    }
+                } else { // isRectangle
+                    if (Collisions.areRectanglesColliding(movementBoundingBox, otherBody)) {
+                        const timeOfCollision = Collisions.getTimeOfCircleOnRectangleCollision(actingBody, otherBody);
+    
+                        if (timeOfCollision !== null) {
+                            moveBodyToPointOfCollision(actingBody, timeOfCollision);
+    
+                            if (actingBody.isElastic) { // assume all rectangles are inelastic
+                                const newVector = Collisions.resolveElasticCircleOnInelasticRectangleCollision(actingBody, otherBody);
+                                actingBody.setVelocity(newVector);
+                            }
+                            
+                            wasCollision = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            if (!wasCollision) {
+                const newX = actingBody.x + actingBody.velocity.x;
+                const newY = actingBody.y + actingBody.velocity.y;
+                actingBody.moveTo(newX, newY);
+            }
+    
+            actingBody.collisionPartner = null;
         }
     }
 }
