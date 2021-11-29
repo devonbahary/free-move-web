91c25c782459462bdaabbd6f6141e85a33a2b3fc
Devon Bahary <devonbahary@gmail.com>
2021-11-28 22:58:02 -0600

    refactors common getDiffPos() logic in tests

--- src/free-move/tests/CollisionEvents.test.ts
+++ src/free-move/tests/CollisionEvents.test.ts
@@ -1,7 +1,7 @@
 import { CircleBody, RectBody } from "../Bodies";
 import { CollisionEvents } from "../CollisionEvents";
 import { Maths } from "../Maths";
-import { BodyType, CollisionEvent } from "../types";
+import { BodyType, CollisionEvent, Vector } from "../types";
 import { Vectors } from "../Vectors";
 import {
     CollisionType,
@@ -14,7 +14,7 @@ const SHOULD_NOT = 'should NOT return any collision event';
 
 const expectZeroTimeOfCollision = (collisionEvent: CollisionEvent) => {
     expect(Maths.roundFloatingPoint(collisionEvent.timeOfCollision) === 0).toBe(true);
-}
+};
 
 describe('CollisionEvents', () => {
     describe('getCollisionEventsInChronologicalOrder()', () => {
@@ -29,6 +29,8 @@ describe('CollisionEvents', () => {
 
         for (const dir of Object.values(Direction)) {
             for (const collisionType of Object.values(CollisionType)) {
+                let getDiffPos: () => Vector;
+                
                 describe(`${collisionType} (${dir})`, () => {
                     beforeEach(() => {
                         switch (collisionType) {
@@ -44,6 +46,7 @@ describe('CollisionEvents', () => {
                                 throw new Error(`unrecognized CollisionType ${collisionType}`);
                         }
                         getCollisionEventsInChronologicalOrder = () => CollisionEvents.getCollisionEventsInChronologicalOrder([bodyA, bodyB], bodyA);
+                        getDiffPos = () => Vectors.subtract(bodyB.center, bodyA.center);
                     });
         
                     describe('invalid collision events', () => {
@@ -103,8 +106,7 @@ describe('CollisionEvents', () => {
                         it(`${SHOULD} when one body is moving towards another body with a movement path that would end within the other body`, () => {
                             TestUtils.moveBodiesApartFromEachOther(bodyA, bodyB, dir);
         
-                            const diffPos = Vectors.subtract(bodyB.center, bodyA.center);
-        
+                            const diffPos = getDiffPos();
                             bodyA.setVelocity(diffPos);
         
                             const [ collisionEvent ] = getCollisionEventsInChronologicalOrder();
@@ -119,9 +121,8 @@ describe('CollisionEvents', () => {
                         it(`${SHOULD} when one body is moving towards another body with a movement path that passes all the way through the other body (prevent tunneling)`, () => {
                             TestUtils.moveBodiesApartFromEachOther(bodyA, bodyB, dir);
         
-                            const diffPos = Vectors.subtract(bodyB.center, bodyA.center);
+                            const diffPos = getDiffPos();
                             const twiceDiffPos = Vectors.rescale(diffPos, Vectors.magnitude(diffPos) * 2);
-        
                             bodyA.setVelocity(twiceDiffPos);
         
                             const [ collisionEvent ] = getCollisionEventsInChronologicalOrder();
@@ -136,8 +137,7 @@ describe('CollisionEvents', () => {
                         it(`${SHOULD_NOT} when one body is moving towards another body that is beyond the reach of the movement path (don't return future events)`, () => {
                             TestUtils.moveBodiesApartFromEachOther(bodyA, bodyB, dir);
         
-                            const diffPos = Vectors.subtract(bodyB.center, bodyA.center);
-        
+                            const diffPos = getDiffPos();
                             bodyA.setVelocity(Vectors.rescale(diffPos, 0.5));
         
                             expectNoCollisionEvents();
@@ -150,7 +150,7 @@ describe('CollisionEvents', () => {
                         });
     
                         it(`${SHOULD_NOT} when a body moves tangentially to another body`, () => {
-                            const diffPos = Vectors.subtract(bodyB.center, bodyA.center);
+                            const diffPos = getDiffPos();
                             const tangentVectors = Vectors.normalVectors(diffPos);
                             
                             for (const tangent of tangentVectors) {
