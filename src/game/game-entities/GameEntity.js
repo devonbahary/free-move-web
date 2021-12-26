cc4f62cca380b5a86e8c27b1c82e34e9d6d7bebb
Devon Bahary <devonbahary@gmail.com>
2021-12-25 18:05:45 -0600

    prevents character movement from exceeding movement speed

--- src/game/game-entities/GameEntity.js
+++ src/game/game-entities/GameEntity.js
@@ -6,7 +6,14 @@ export class GameEntity {
     }
 
     move(movementVector) {
-        this.body.setVelocity(Vectors.rescale(movementVector, this.movementSpeed));
+        const maxMag = Math.max(Vectors.magnitude(this.body.velocity), this.movementSpeed);
+        const movementForce = Vectors.rescale(movementVector, this.movementSpeed); 
+        
+        this.body.applyForce(movementForce);
+        // never let character movement exceed movement speed
+        if (Vectors.magnitude(this.body.velocity) > maxMag) {
+            this.body.setVelocity(Vectors.rescale(this.body.velocity, maxMag));
+        }
     }
 
     moveTo(positionVector) {
